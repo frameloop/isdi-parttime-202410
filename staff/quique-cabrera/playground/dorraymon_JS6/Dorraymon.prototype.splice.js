d5071f00e3f29162775edf194353c6c7497f0c01
Quique <frameloop@gmail.com>
2024-11-23 20:55:25 +0100

    add Dorraymon.prototype.splice with REACT  #8

--- staff/quique-cabrera/playground/dorraymon_JS6/Dorraymon.prototype.splice.js
+++ staff/quique-cabrera/playground/dorraymon_JS6/Dorraymon.prototype.splice.js
@@ -1,29 +1,59 @@
-function Dorraymon() {
-    this.length = 0
+console.log('TEST Dorraymon prototype splice')
+
+class Dorraymon {
+    constructor() {
+        this.length = 0
+    }
+
+    splice(start, deleteCount, ...items) {
+
+        let merged = {} // Dorraymon final
+        let index = 0 // Nuevo indice
+
+        // Copiar elementos iniciales antes de start
+        for (let i = 0; i< start; i ++){
+        merged[index] = this[i]
+        index++
+        }
+
+        //Insertar los nuevos elementos
+        for (let i=0; i<items.length; i++){    
+        merged[index] = items[i]
+        index++
+        }
+
+        // Mover elementos existentes despues del borrado
+        for (let i = start + deleteCount; i < this.length; i++) {
+        merged[index] = this[i]
+        index++
+        }
+
+        // Actualizar la logitud del Dorraymon
+        merged.length = index
+
+        // Paras el orden al Dorraymon
+        for (let newIndex in merged){
+        this[newIndex] = merged[newIndex]
+        }
+
+        return this
+    }
 }
 
-console.log('TEST Dorraymon.prototype.splice')
+console.log('CASE add abril and mayo in months')
 
-Dorraymon.prototype.splice = function (index, pop, element) {
-    this[index - 1] = element
-    // if (pop > 0)
-    //     delete this[pop]
-    return this
+{
+    const months = new Dorraymon
+    months[0] = 'january'
+    months[1] = 'february'
+    months[2] = 'march'
+    months[3] = 'june'
+    months[4] = 'july'
+    months.length = 5
+
+    months.splice(3, 0, 'april', 'may')
+    // Dorraymon {0: 'january', 1: 'february', 2: 'march', 3: 'april', 4: 'may', 5: 'june', 6: 'july', length: 7}
 }
 
-console.log('CASE  insert element in Dorraymon')
 
-var notes = new Dorraymon
-notes[0] = 'Do'
-notes[1] = 'Re'
-notes[2] = 'Fa'
-notes[3] = 'Sol'
-notes[4] = 'Si'
-notes.length = 5
 
-notes.splice(3, 0, 'Mi')
-console.log(notes)
-
-notes.splice(5, 0, 'La')
-console.log(notes)
-// Dorray ["Do", "Re", "Mi", "Fa", "Sol", "Si"]
