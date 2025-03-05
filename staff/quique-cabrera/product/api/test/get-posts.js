e63a653ae6bfcb850363936ba6132f94dcbce051
Quique <frameloop@gmail.com>
2025-03-05 18:02:35 +0100

    minors corrections #27

--- staff/quique-cabrera/product/api/test/get-posts.js
+++ staff/quique-cabrera/product/api/test/get-posts.js
@@ -1,4 +1,4 @@
-fetch('http://localhost:8080/posts', {
+fetch('http://localhost:8080/users', {
     method: 'GET',
     headers: Autorization: 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiI2NzkyYWE2ZmQ3NWI3NjdhNTUyZTJkYzMiLCJpYXQiOjE3MzkzNzUxNDN9.NxHsivUTSW2N8faRDxsXCW4hlfUHlHlzG0IeJ9kp1r0'
 })
